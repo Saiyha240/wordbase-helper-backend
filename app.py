ac754a1a069fa0c56723492619f7aa427bc4a556
Mark Cinco <mark5cinco92@gmail.com>
2018-06-08 17:49:50 +0900

    Finish add_tile and print for WordBase object

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import json
 from chalice import Chalice
 
 from chalicelib.trie import TrieNode, TrieBase
-from chalicelib.wordbase import WordBase, Letter
 
 app = Chalice(app_name='wordbase-helper-backend')
 
@@ -25,64 +24,3 @@ def index():
     dictionary = get_dictionary()
 
     return {'hello': 'world'}
-
-
-data = {
-    "rowCount": "13",
-    "colCount": "10",
-    "markedMap": [
-        {
-            "xCoor": 0,
-            "yCoor": 0
-        },
-        {
-            "xCoor": 1,
-            "yCoor": 0
-        },
-        {
-            "xCoor": 2,
-            "yCoor": 0
-        },
-        {
-            "xCoor": 3,
-            "yCoor": 0
-        },
-        {
-            "xCoor": 4,
-            "yCoor": 0
-        },
-        {
-            "xCoor": 5,
-            "yCoor": 0
-        },
-        {
-            "xCoor": 6,
-            "yCoor": 0
-        },
-        {
-            "xCoor": 7,
-            "yCoor": 0
-        },
-        {
-            "xCoor": 8,
-            "yCoor": 0
-        },
-        {
-            "xCoor": 9,
-            "yCoor": 0
-        }
-    ],
-    "cellsString": "ectrygamylkiehrantghtfpdodeistosfauidcpicetbroramerilgoemseraoncnutfgodecisgytasilotneirnclrfmarmscenatopmseliyliaryecsnwhlplrptum"
-}
-
-wb = WordBase(data['cellsString'])
-
-for row_num in range(0, wb.rows):
-    # print(row_num)
-    for col_num in range(0, wb.columns):
-        letter = wb.data[row_num * wb.columns + col_num]
-        wb.add_tile(Letter(letter, row_num, col_num))
-        # print(col_num)
-        pass
-
-print(wb)
--- chalicelib/wordbase.py
+++ chalicelib/wordbase.py
@@ -8,6 +8,9 @@ class Letter(object):
     def add_adjacent_letter(self, letter_obj):
         self.adjacent_letters.append(letter_obj)
 
+    def __str__(self) -> str:
+        return self.char
+
 
 class WordBase(object):
     def __init__(self, data: str, rows: int = 13, columns: int = 10) -> None:
@@ -17,6 +20,17 @@ class WordBase(object):
         self.data = data
 
     def add_tile(self, letter: Letter):
-        letter.adjacent_letters = [(letter.x_coor + i, letter.y_coor + j) for i in (-1, 0, 1) for j in (-1, 0, 1) if
-                                   i != 0 or j != 0]
+        for i in (-1, 0, 1):
+            for j in (-1, 0, 1):
+                adj_x = letter.x_coor + i
+                adj_y = letter.y_coor + j
+                if (i != 0 or j != 0) and (0 <= adj_x <= self.columns and 0 <= adj_y <= self.rows):
+                    letter.add_adjacent_letter((adj_x, adj_y))
+
         self.tiles[(letter.x_coor, letter.y_coor)] = letter
+
+    def print(self):
+        for row in range(self.rows):
+            for column in range(self.columns):
+                print('({0}) '.format(self.tiles[(row, column)]), end='')
+            print()
